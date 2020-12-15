55372bb9c361ac8d6763b1e62b346ed420cb7183
Shivam Kalra <shivamkalra98@gmail.com>
2020-12-15 19:32:13 +0530

    submit todo on enter key

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { InputGroup, Button } from "@blueprintjs/core";
 function Form({ input, submit, inputText }) {
+    const enterHandler = (e) => {
+        if(e.key === "Enter"){
+            submit();
+        }
+    };
     return (
         <div className="Form">
             <InputGroup
                 value={inputText}
                 className="form-input"
                 onChange={input}
+                onKeyPress={enterHandler}
             />
             <Button onClick={submit}>Submit</Button>
         </div>
